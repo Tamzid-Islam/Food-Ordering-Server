091153eb9c78554ef4b9178b5de8fe88120537ea
Tamzid-Islam <tamzid.islam321@gmail.com>
2023-01-21 16:39:02 +0600

    gitignore fixed

--- server.js
+++ server.js
@@ -19,61 +19,14 @@ async function run() {
         const serviceCollection = database.collection('FoodList');
         // const myOrderCollection = database.collection('Order');
         console.log('database connected');
-        const docs = [
-            {
-                id: "d1",
-                src: "./image/dish1.webp",
-                name: "Yam and egg sauce",
-                price: 500,
-            },
-            {
-                id: "d2",
-                src: "./image/dish2.webp",
-                name: "Jollof rice and chicken",
-                price: 300,
-            },
-            {
-                id: "d3",
-                src: "./image/dish3.webp",
-                name: "Porridge beans",
-                price: 200,
-            },
-            {
-                id: "d4",
-                src: "./image/dish4.webp",
-                name: "Semo and egusi soup",
-                price: 350,
-            },
-            {
-                id: "d5",
-                src: "./image/dish5.webp",
-                name: "Amala and ewedu soup",
-                price: 450.0,
-            },
-            {
-                id: "d6",
-                src: "./image/dish6.webp",
-                name: "Eba and okra soup",
-                price: 300.0,
-            },
-            {
-                id: "d7",
-                src: "./image/dish1.webp",
-                name: "Abradaka dabra",
-                price: 250.0
-            }
-        ];
-        const options = { ordered: true };
-        //const result = await serviceCollection.insertMany(docs, options)
-        //console.log(`${result.insertedCount} documents were inserted`);
 
-        // getting from database 
+        // getting from database
         app.get('/foodlist', async (req, res) => {
             const cursor = serviceCollection.find({})
             const services = await cursor.toArray();
             res.send(services)
         });
-        // GET SINGLE API
+        //get single data
         app.get('/foodlist/:id', async (req, res) => {
             const id = req.params.id;
             console.log(id);
@@ -88,9 +41,6 @@ async function run() {
             const result = await myOrderCollection.find(query).toArray();
             res.json(result);
         })
-
-        //add many item
-
         // add data to cart collection with additional info
         app.post('/order/add', async (req, res) => {
             const order = req.body;
